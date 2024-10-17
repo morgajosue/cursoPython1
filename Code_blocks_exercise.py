fe194eea9bb1da7f323621a2f8bfa544e61b2351
Josue Morga <153125762+morgajosue@users.noreply.github.com>
2024-10-17 09:56:28 -0600

    Create Code_blocks_exercise.py
    Codigo inicial

---
+++ Code_blocks_exercise.py
@@ -0,0 +1,11 @@
+blocks = int(input("Ingresa el número de bloques: "))
+minus = 1
+height = 0
+
+while(blocks >= minus)
+  blocks -= minus
+  minus += 1
+  height += 1
+
+print("La altura de la pirámide:", height)
+
